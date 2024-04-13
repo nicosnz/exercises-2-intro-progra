73c937d7ca6e169358227e3b950e99b39ff2a20d
Emanuel Oly <159573775+nicosnz@users.noreply.github.com>
2024-04-13 15:26:01 -0400

    modificando ejer6

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -25,10 +25,10 @@ void exercise_5(int n, int k) {
 }
 
 int exercise_6(int n) {
- const auto expected=0;
- while (n>0){
-  expected= expected + n%10;
-  n= n/10;
+  auto expected=0;
+  while (n>0){
+    expected= expected + n%10;
+    n= n/10;
  }
  cout<<expected<<endl;
  cout<<n<<endl;
