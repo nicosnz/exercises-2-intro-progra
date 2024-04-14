8cba4e0b2cafd141462d9d931b1d34756dffc265
Emanuel Oly <159573775+nicosnz@users.noreply.github.com>
2024-04-14 18:14:22 -0400

    ejer15;primera prueba

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -133,7 +133,26 @@ string exercise_14(int n) {
 }
 
 void exercise_15(int decimal) {
-  // TODO: YOUR CODE HERE
+  int binario[8];
+
+  if(decimal==0){
+    cout<<"0"<<endl;
+
+  }
+  else if(decimal==1){
+    cout<<"1"<<endl;
+  }
+
+  else{
+     for(int i=0;i<8;i++){
+      binario[i]=decimal%2;
+      decimal/=2;
+      
+  }
+  for(int i=7;i>=0;i--){
+      cout<<binario[i];
+  }
+  
 }
 
 void exercise_16(int divident, int divider) {
