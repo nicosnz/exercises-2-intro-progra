907c5685755850c265b6c67a57ebf4c2304d1ff0
Emanuel Oly <159573775+nicosnz@users.noreply.github.com>
2024-04-14 21:22:58 -0400

    Ejer3; primer intento

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -13,7 +13,16 @@ void exercise_2(string s1) {
 }
 
 void exercise_3(string s1) {
-  // TODO: YOUR CODE HERE
+   for(int i=0;i<s1.size();i++){
+        if(s1[i]==1){
+            cout<<"Om-nom-nom :P"<<endl;
+            
+        }
+        else if(s1[i]==0){
+            cout<<"No cake"<<endl;
+            break;
+        }
+     
 }
 
 void exercise_4(int n) {
