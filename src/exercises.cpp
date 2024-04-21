a4481aede8addabcc2bb6c9cc8326ca5db6202d8
Emanuel Oly <159573775+nicosnz@users.noreply.github.com>
2024-04-21 12:29:30 -0400

    ejer 15

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -80,13 +80,13 @@ void exercise_4(int n) {
 
 void exercise_5(int n, int k) {
   for(int a=1;a<n;a++){
-        cout<<"   ";
+        cout<<" ";
     }
     
     for(int i=1;i<=k;i++){
         cout<<i;
         if(i<10){
-            cout<<"   ";
+            cout<<"  ";
         } 
         else{
             cout<<"   "; 
